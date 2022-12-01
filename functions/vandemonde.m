b5a0c96103659a2e7f732400744db80bd2a32371
j478 <jakedavies11@ku.edu>
2022-12-01 12:32:21 -0600

    finished implementation of vandemonde

---
+++ functions/vandemonde.m
@@ -0,0 +1,30 @@
+function [polynomial] = vandemonde(orderedPairs)
+%VANDEMONDEMATRIX Summary of this function goes here
+%   Detailed explanation goes here
+n = length(orderedPairs);
+ts = orderedPairs(:,1);
+ys = orderedPairs(:,2);
+
+A = zeros(n);
+for i=1:n
+    for j=1:n
+        if j == 1
+            A(i,j) = 1;
+        else
+            A(i,j) = ts(i).^(j-1);
+        end
+    end
+end
+
+
+
+coeffs = lsqr(A, ys, 1e-6, 500);
+
+polyString = "" + coeffs(1);
+for i=1:n-1
+    polyString = polyString + " + (" + coeffs(i+1) + "*(t.^" + i + "))";
+end
+
+polynomial = inline(polyString, "t");
+end
+
