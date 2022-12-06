0400ac86c28baec220c4475d81e79332ef759626
Eva Morrison <eva.morrison33@ku.edu>
2022-12-05 21:52:04 -0600

    plot cubics

--- functions/bezier.m
+++ functions/bezier.m
@@ -12,6 +12,7 @@ function [coefficients] = bezier(n,endPoint,leftGuide,rightGuide)
 %       coefficients: nx8 matrix, coefficients for bezier curve [a1, a2,
 %       a3, a4, b1, b2, b3, b4]
 %   Detailed explanation goes here
+t = linspace(0,1,20);
 coefficients = zeros(n,8);
 for i = 1:n
     coefficients(i,1) = endPoint(1,i);
@@ -22,5 +23,15 @@ for i = 1:n
     coefficients(i,6) = 3 * (leftGuide(2,i) - endPoint(2,i));
     coefficients(i,7) = 3 * (endPoint(2,i) + rightGuide(2,i) - 2 * leftGuide(2,i));
     coefficients(i,8) = endPoint(1,(i+1)) - endPoint(1,i) + 3 * leftGuide(1,i) - 3 * rightGuide(1,i);
+
+    funcx = @(t) coefficients(i,1) + coefficients(i,2)*t + coefficients(i,3)*t.^2+coefficients(i,4)*t.^3;
+    funcy = @(t) coefficients(i,5) + coefficients(i,6)*t + coefficients(i,7)*t.^2+coefficients(i,8)*t.^3;
+    x = funcx(t);
+    y = funcy(t);
+    plot(x,y);
+    hold on;
+    
+end
+hold off;
 end
 
