b0b2387c889df862e44b53968101af6eb5bdae94
Eva Morrison <eva.morrison33@ku.edu>
2022-12-07 11:07:10 -0600

    add comments to plotEllipse

--- functions/plotEllipse.m
+++ functions/plotEllipse.m
@@ -1,19 +1,24 @@
 function [] = plotEllipse(n)
-t = linspace(0,2*pi,n);
+% creates the points that will passed on to the bezier function. 
+% INPUTS:
+%   n: number of points that will be generated
+% OUTPUTS:
+%   null: nothing is returned
+t = linspace(0,2*pi,n); % generate n equally spaced points on [0,2pi]
 
-ellipseX = @(t) 5*cos(3*t);
+ellipseX = @(t) 5*cos(3*t); % ellipse parametric function
 ellipseY = @(t) 2*sin(3*t);
 
-ellipseXprime = @(t) -15*sin(3*t);
+ellipseXprime = @(t) -15*sin(3*t);% first derivative of ellipse
 ellipseYprime = @(t) 6*cos(3*t);
 
-x_coord = ellipseX(t);
+x_coord = ellipseX(t); % create n points
 y_coord = ellipseY(t);
 
-alpha = ellipseXprime(t);
+alpha = ellipseXprime(t); % create the tranformations for the guide points
 beta = ellipseYprime(t);
 
-endPoints = [x_coord;y_coord].';
+endPoints = [x_coord;y_coord].'; % create nx2 arrays to be passed to bezier
 leftGuides = [x_coord+alpha;y_coord+beta].';
 rightGuides = [x_coord-alpha;y_coord-beta].';
 
