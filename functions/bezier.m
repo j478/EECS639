f0b465b8cea4cc7a239371017463a38125b816f9
Eva Morrison <eva.morrison33@ku.edu>
2022-12-06 22:41:48 -0600

    slight changes to bezier

--- functions/bezier.m
+++ functions/bezier.m
@@ -1,12 +1,12 @@
 function [coefficients] = bezier(n,endPoint,leftGuide,rightGuide)
 %BEZIER Summary of this function goes here
 %   INPUT: 
-%       n: int, number of points
-%       endPoint: 2xn matrix, coordinates from endpoints on original
+%       n: int, number of curves to be constructed
+%       endPoint: nx2 matrix, coordinates from endpoints on original
 %       function
-%       leftGuide: 2x(n-1) matrix, coordinates from first derivative on
+%       leftGuide: (n-1)x2 matrix, coordinates from first derivative on
 %       left derivative
-%       rightGuide: 2x(n-1) matrix, coordinates from first derivative on
+%       rightGuide: (n-1)x2 matrix, coordinates from first derivative on
 %       right guide
 %   OUTPUT:
 %       coefficients: nx8 matrix, coefficients for bezier curve [a1, a2,
@@ -15,14 +15,14 @@ function [coefficients] = bezier(n,endPoint,leftGuide,rightGuide)
 t = linspace(0,1,20);
 coefficients = zeros(n,8);
 for i = 1:n
-    coefficients(i,1) = endPoint(1,i);
-    coefficients(i,2) = 3 * (leftGuide(1,i) - endPoint(1,i));
-    coefficients(i,3) = 3 * (endPoint(1,i) + rightGuide(1,i) - 2 * leftGuide(1,i));
-    coefficients(i,4) = endPoint(1,(i+1)) - endPoint(1,i) + 3 * leftGuide(1,i) - 3 * rightGuide(1,i);
-    coefficients(i,5) = endPoint(2,i);
-    coefficients(i,6) = 3 * (leftGuide(2,i) - endPoint(2,i));
-    coefficients(i,7) = 3 * (endPoint(2,i) + rightGuide(2,i) - 2 * leftGuide(2,i));
-    coefficients(i,8) = endPoint(1,(i+1)) - endPoint(1,i) + 3 * leftGuide(1,i) - 3 * rightGuide(1,i);
+    coefficients(i,1) = endPoint(i,1);
+    coefficients(i,2) = 3 * (leftGuide(i,1) - endPoint(i,1));
+    coefficients(i,3) = 3 * (endPoint(i,1) + rightGuide(i,1) - 2 * leftGuide(i,1));
+    coefficients(i,4) = endPoint((i+1),1) - endPoint(i,1) + 3 * leftGuide(i,1) - 3 * rightGuide(i,1);
+    coefficients(i,5) = endPoint(i,2);
+    coefficients(i,6) = 3 * (leftGuide(i,2) - endPoint(i,2));
+    coefficients(i,7) = 3 * (endPoint(i,2) + rightGuide(i,2) - 2 * leftGuide(i,2));
+    coefficients(i,8) = endPoint((i+1),2) - endPoint(i,2) + 3 * leftGuide(i,2) - 3 * rightGuide(i,2);
 
     funcx = @(t) coefficients(i,1) + coefficients(i,2)*t + coefficients(i,3)*t.^2+coefficients(i,4)*t.^3;
     funcy = @(t) coefficients(i,5) + coefficients(i,6)*t + coefficients(i,7)*t.^2+coefficients(i,8)*t.^3;
